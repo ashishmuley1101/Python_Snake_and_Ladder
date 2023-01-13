efddc2f1ba6d1db7d0b873b808c2d96937f91a77
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-13 16:22:52 +0530

    # UC 6: The number of times the dice was played to win the game and also the position after every die role.

--- main.py
+++ main.py
@@ -4,38 +4,56 @@ class SnakeNLadder:
 
 	STARTINGPOSITION = 0;
 	ENDINGPOSITION = 100;
+	roll_die = 0
+
+
+
 
 	def start_game(self):
+
 		current_position = 0
 		print("Start Position Of Player :", current_position)
 
-# UC 5: The player gets to exact winning position 100.
+# UC 6: The number of times the dice was played to win the game and also the position after every die role.
 
 		if current_position < SnakeNLadder.ENDINGPOSITION:
+
+				count = 0
+
 				while current_position < SnakeNLadder.ENDINGPOSITION:
+
 					roll_die = random.randint(1, 6)
+
 					option = random.randint(1, 3)
+
 					if option == 1:
 						if current_position + roll_die <= SnakeNLadder.ENDINGPOSITION :
 							current_position = current_position + roll_die;
 							print("Dies Roll  : ", roll_die)
 							print("Ladder : ", option)
 							print("Current position : ", current_position)
+
 					elif option == 2:
 						if current_position - roll_die >= SnakeNLadder.STARTINGPOSITION:
 							current_position = current_position - roll_die
 							print("Dies Roll : ", roll_die)
 							print("Snake : ", option)
 							print("Current position : ", current_position)
+
 					else:
 						if current_position == current_position:
 								current_position = current_position
 								print("Dies Roll : ", roll_die)
 								print("No Play : ", option)
 								print("Current position : ", current_position)
+
+					count += 1
+
 				if current_position == 100:
-					print("Player Winner at position 100.")
+					print("\nPlayer Winner at position 100.")
+
+				print("Number of times the dice was played : ", count)
 
 print("Welcome to Snake and Ladder..!")
-uc1 = SnakeNLadder()
-uc1.start_game()
+sl1 = SnakeNLadder()
+sl1.start_game()
