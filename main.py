8a1ae3f7ee422a9df562d361f1ea93616febab4a
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-13 12:13:13 +0530

    UC2 : The Player rolls the die to get a number between 1 to 6.

--- main.py
+++ main.py
@@ -8,7 +8,20 @@ class SnakeNLadder:
 		player1 = 0
 		roll_die = random.randint(1, 6)
 		print("Start Position Of Player1 :", player1)
-		print("Player roll die : ", roll_die)
+		print("Die Roll : ", roll_die)
+
+# UC 3: The Player then checks for a Option. They are No Play,Ladder or Snake.
+
+		option = random.randint(1, 3)
+		if option == 1:
+			player1 = player1 + roll_die
+			print("Player got ladder : ", player1)
+		elif option == 2:
+			player1 = player1 - roll_die
+			print("Player got snake : ", player1)
+		else:
+			player1 = player1
+			print("Player not played : ", player1)
 
 print("Welcome to Snake and Ladder..!")
 uc1 = SnakeNLadder()
