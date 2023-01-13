1b5c94c108b1af0d32117b046fb821e0c8c9988d
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-13 14:29:04 +0530

    UC4 The Player reaches the winning position 100.

--- main.py
+++ main.py
@@ -2,25 +2,43 @@
 import random
 class SnakeNLadder:
 
+	STARTINGPOSITION = 0;
+	ENDINGPOSITION = 100;
+
+
+	# def __init__(self, STARTINGPOSITION,ENDINGPOSITION):
+	# 	self.ENDINGPOSITION = ENDINGPOSITION
+	# 	self.STARTINGPOSITION = STARTINGPOSITION
 
 	def start_game(self):
-		player1 = 0
-		roll_die = random.randint(1, 6)
-		print("Start Position Of Player1 :", player1)
-		print("Die Roll : ", roll_die)
+		current_position = 0
+		print("Start Position Of Player :", current_position)
 
-# UC 3: The Player then checks for a Option. They are No Play,Ladder or Snake.
+# UC 4: The Player reaches the winning position 100.
 
-		option = random.randint(1, 3)
-		if option == 1:
-			player1 = player1 + roll_die
-			print("Player got ladder : ", player1)
-		elif option == 2:
-			player1 = player1 - roll_die
-			print("Player got snake : ", player1)
-		else:
-			player1 = player1
-			print("Player not played : ", player1)
+		if current_position < SnakeNLadder.ENDINGPOSITION:
+			# for position in range(SnakeNLadder.ENDINGPOSITION):
+				while current_position < SnakeNLadder.ENDINGPOSITION:
+					roll_die = random.randint(1, 6)
+					option = random.randint(1, 3)
+					if option == 1:
+						if current_position + roll_die <= SnakeNLadder.ENDINGPOSITION :
+							current_position = current_position + roll_die;
+							print("Dies roll : ", roll_die)
+							print("Ladder : ", option)
+							print("Current position : ", current_position)
+					elif option == 2:
+						if current_position - roll_die >= SnakeNLadder.STARTINGPOSITION:
+							current_position = current_position - roll_die
+							print("Dies roll : ", roll_die)
+							print("Snake : ", option)
+							print("Current position : ", current_position)
+					else:
+						if current_position == current_position:
+								current_position = current_position
+								print("Dies roll : ", roll_die)
+								print("No Play : ", option)
+								print("Current position : ", current_position)
 
 print("Welcome to Snake and Ladder..!")
 uc1 = SnakeNLadder()
