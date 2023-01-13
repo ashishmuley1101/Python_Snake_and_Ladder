a88ded86825e1c4fb0cc31a6313d473aea9af4e6
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-13 11:02:23 +0530

    UC1 : snake and ladder game played with single player at start position 0

---
+++ main.py
@@ -0,0 +1,11 @@
+class SnakeNLadder:
+
+# UC1 : snake and ladder game played with single player at start position 0
+
+	 def start_game(self):
+		 self.player1 = 0
+		 print("Start Position Of Player1 :", self.player1)
+
+print("Welcome to Snake and Ladder..!")
+uc1 = SnakeNLadder()
+uc1.start_game()
